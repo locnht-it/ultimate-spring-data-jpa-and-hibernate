058022509736754ebc9093280b7b1dd8ee01f1a0
locnht-it <locnht.it@gmail.com>
2024-04-04 09:04:48 +0700

    first commit

---
+++ src/main/java/com/locnht/UltimateSpringDataJpaAndHibernate/UltimateSpringDataJpaAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.locnht.UltimateSpringDataJpaAndHibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UltimateSpringDataJpaAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UltimateSpringDataJpaAndHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/locnht/UltimateSpringDataJpaAndHibernate/UltimateSpringDataJpaAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.locnht.UltimateSpringDataJpaAndHibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UltimateSpringDataJpaAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
